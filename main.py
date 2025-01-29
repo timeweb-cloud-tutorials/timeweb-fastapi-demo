5213b7c6dd1b3682af77f6b6e36c5b7f47c1adeb
tecs <sdurmanov@gmail.com>
2025-01-29 13:57:32 +0300

    minor

--- main.py
+++ main.py
@@ -215,5 +215,5 @@ def delete_variable(key: str, db: Session = Depends(get_db)):
 
 
 # For production (Timeweb apps)
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
